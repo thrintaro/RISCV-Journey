db82208003df3a1ff754a4b3d4c66514543db955
ANUJ MISHRA <anujm7978@gmail.com>
2026-04-13 05:13:03 +0000

    First simulation - half adder verified working

---
+++ testbench/half_adder_tb.v
@@ -0,0 +1,16 @@
+module half_adder_tb;
+    reg a, b;
+    wire sum, carry;
+    
+    half_adder uut(.a(a), .b(b), .sum(sum), .carry(carry));
+    
+    initial begin
+        $dumpfile("sim/half_adder.vcd");
+        $dumpvars(0, half_adder_tb);
+        a=0; b=0; #10;
+        a=0; b=1; #10;
+        a=1; b=0; #10;
+        a=1; b=1; #10;
+        $finish;
+    end
+endmodule
